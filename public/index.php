e758d2bfbb04d3281974767e16a661962d904fb6
realjjaveweb <jave.web@seznam.cz>
2025-05-05 01:10:05 +0200

    [RD] public routing - .htaccess & index.php (using custom RouteFinder and Slim's app & error middleware)

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+// error reporting&display OFF by default
+error_reporting(0);
+ini_set('display_errors', '0');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Helper\RouteFinder;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+(new RouteFinder())->findAndRegisterControllerRoutes($app);
+
+// prettier http exceptions <3
+$app->addErrorMiddleware(
+    displayErrorDetails: false,
+    logErrors: true,
+    logErrorDetails: true,
+    logger: null
+);
+
+$app->run();
