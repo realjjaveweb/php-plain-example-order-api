0e231e88faddca12ed2937d3f3f3d138e2a73127
realjjaveweb <jave.web@seznam.cz>
2025-05-05 01:07:33 +0200

    [RD] AbstractController - preparation for RouteFinder

---
+++ src/Controller/Common/AbstractController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Common;
+
+use Slim\App;
+use App\Controller\Common\Attribute\Route;
+use App\Controller\Common\Enum\HttpMethod;
+
+abstract class AbstractController
+{
+    final public function registerRoutes(App $app): void
+    {
+        foreach ($this->getRouteAttributes() as $classMethod => $route) {
+            $requestMethodCases = \is_array($route->method) ? $route->method : [$route->method];
+            $requestMethodStrings = \array_map(
+                static fn (HttpMethod $requestMethod) => $requestMethod->value,
+                $requestMethodCases,
+            );
+
+            $app->map(
+                methods: $requestMethodStrings,
+                pattern: $route->path,
+                callable: $this->$classMethod(...),
+            );
+        }
+    }
+
+    /** @return list<Route> */
+    final protected function getRouteAttributes(): array
+    {
+        $attrReflection = new \ReflectionClass($this);
+
+        $routes = [];
+        foreach ($attrReflection->getMethods(\ReflectionMethod::IS_PUBLIC) as $classMethod) {
+            foreach ($classMethod->getAttributes(Route::class) as $routeAttr) {
+                $routes[$classMethod->name] = $routeAttr->newInstance();
+            }
+        }
+
+        return $routes;
+    }
+}
